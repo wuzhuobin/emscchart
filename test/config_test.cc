28d20ad0a6171ecb81315e60f4718c2c50d105dd
wuzhuobin <zhuobin.wu@philips.com>
2024-07-24 14:05:37 +0800

    Update unittests in config

--- test/config_test.cc
+++ test/config_test.cc
@@ -1,3 +1,22 @@
+#include "config.h"
 #include <gtest/gtest.h>
 
-GTEST_TEST(AAA, aaa) { EXPECT_EQ(1, 1); }
+class ConfigTest : public testing::Test {};
+
+TEST_F(ConfigTest, UpdateShouldResetOptions) {
+  using OptionsByte = std::array<std::byte, sizeof(emscchart::Options)>;
+
+  emscchart::Config config{emscchart::Configuration()};
+  config.Update();
+  auto updated_options_bytes =
+      reinterpret_cast<OptionsByte const&>(config.Options());
+
+  emscchart::Options initial_options{};
+  auto initial_options_bytes =
+      reinterpret_cast<OptionsByte const&>(initial_options);
+
+  for (auto i = 0; i < sizeof(emscchart::Options); i++) {
+    EXPECT_EQ(updated_options_bytes[i], initial_options_bytes[i])
+        << "where i is " << i;
+  }
+}
